19ffab85a06524c196aff10a78fe47da99d6b47f
aaronguarnes <aaronguarnes@gmail.com>
2020-08-14 19:09:51 +0800

    first commit

---
+++ fb-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyClx2ZV9qn9IcoQ5jFqNH5jjxSCyUDxGdo",
+    authDomain: "aonresume.firebaseapp.com",
+    databaseURL: "https://aonresume.firebaseio.com",
+    projectId: "aonresume",
+    storageBucket: "aonresume.appspot.com",
+    messagingSenderId: "39141515237",
+    appId: "1:39141515237:web:feaa5bda61326b3d2d190c"
+  };
